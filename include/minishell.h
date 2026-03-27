fc8207752af2ce249ce2adfa002ff525cf4f3b6a
Htut Ko Ko <htutkoko1994@gmail.com>
2026-03-27 20:42:59 +0700

    Standardized header alignment to column 17 to resolve MISALIGNED_FUNC_DECL

--- include/minishell.h
+++ include/minishell.h
@@ -120,102 +120,99 @@ typedef struct s_command
 /*
  * Parsing & Lexing
  */
-t_token					*tokenize(char *line, t_exec_params *params);
-t_command				*parse_input(char *line, char **envp, int *last_status);
-t_command				*parse_tokens(t_token *tokens);
-int						validate_syntax(t_token *tokens);
-int						is_whitespace(char c);
-t_token					*new_token(char *value, t_token_type type);
-void					append_token(t_token **head, t_token *new_t);
-void					free_tokens(t_token *tokens);
-int						print_err(char *token);
-char					*expand_status(char *val, t_exec_params *params);
-t_command				*new_command(void);
-void					add_argument(t_command *cmd, char *arg);
-void					add_redirection(t_command *cmd, t_token *token,
-							t_token *file_token);
-int						get_word_end(char *line, int i);
-int						handle_word(char *line, int i, t_token **head,
-							t_exec_params *params);
-t_token					*expand_wildcard(char *pattern);
-int						has_unquoted_wildcard(char *str);
-int						has_unquoted_var(char *str);
-int						count_wildcard_matches(char *pattern);
-void					expand_cmd_wildcards(t_command *cmd);
-char					*append_char(char *res, char c);
-t_token					*extract_subtokens(t_token *start, t_token *end);
-void					sort_strings(char **arr, int count);
-char					*prepare_pattern(char *pattern);
-void					clean_marker(char *pattern);
-int						get_expanded_count(char *arg);
-void					fill_wildcard_list(char *arg, char **new_args, int *c);
-void					toggle_quotes(char c, bool *s_quote, bool *d_quote);
-char					*apply_expansion(char *res, char *var_val);
-bool					is_expandable(char *v, int i, bool sq);
-void					handle_status_quotes(char c, bool q[3], int *i);
-char					*expand_heredoc_line(char *line, t_exec_params *params);
-char					*handle_expansion(char *val, int *i, t_exec_params *p,
-							bool dq);
+t_token			*tokenize(char *line, t_exec_params *params);
+t_command		*parse_input(char *line, char **envp, int *last_status);
+t_command		*parse_tokens(t_token *tokens);
+int				validate_syntax(t_token *tokens);
+int				is_whitespace(char c);
+t_token			*new_token(char *value, t_token_type type);
+void			append_token(t_token **head, t_token *new_t);
+void			free_tokens(t_token *tokens);
+int				print_err(char *token);
+char			*expand_status(char *val, t_exec_params *params);
+t_command		*new_command(void);
+void			add_argument(t_command *cmd, char *arg);
+void			add_redirection(t_command *cmd, t_token *token,
+					t_token *file_token);
+int				get_word_end(char *line, int i);
+int				handle_word(char *line, int i, t_token **head,
+					t_exec_params *params);
+t_token			*expand_wildcard(char *pattern);
+int				has_unquoted_wildcard(char *str);
+int				has_unquoted_var(char *str);
+int				count_wildcard_matches(char *pattern);
+void			expand_cmd_wildcards(t_command *cmd);
+char			*append_char(char *res, char c);
+t_token			*extract_subtokens(t_token *start, t_token *end);
+void			sort_strings(char **arr, int count);
+char			*prepare_pattern(char *pattern);
+void			clean_marker(char *pattern);
+int				get_expanded_count(char *arg);
+void			fill_wildcard_list(char *arg, char **new_args, int *c);
+void			toggle_quotes(char c, bool *s_quote, bool *d_quote);
+char			*apply_expansion(char *res, char *var_val);
+bool			is_expandable(char *v, int i, bool sq);
+void			handle_status_quotes(char c, bool q[3], int *i);
+char			*expand_heredoc_line(char *line, t_exec_params *params);
+char			*handle_expansion(char *val, int *i, t_exec_params *p, bool dq);
 
 /*
  * Execution
  */
-char					*find_command_path(char *cmd, char **envp);
-void					execute_commands(t_command *cmd_list, char ***envp,
-							int *last_status);
-void					handle_process_loop(t_command *cmd, char ***envp,
-							int *last_status);
-int						is_builtin(char *cmd);
-int						execute_builtin(t_command *cmd, char ***envp,
-							int *last_status);
-void					run_single_builtin(t_command *cmd, char ***envp,
-							int *last_status);
-int						handle_redirections(t_command *cmd,
-							t_exec_params *params);
-void					wait_for_children(pid_t last_pid, int *last_status);
-void					execute_command_node(t_command *curr, char ***envp,
-							int *last_status);
-void					child_process(t_command *cmd, t_exec_params *params);
-int						handle_in(t_redir *redir);
-int						handle_out(t_redir *redir);
-int						handle_heredoc(t_redir *redir, t_exec_params *params);
-int						is_dir(char *path);
-void					exec_script(char *path, char **args, char **envp);
+char			*find_command_path(char *cmd, char **envp);
+void			execute_commands(t_command *cmd_list, char ***envp,
+					int *last_status);
+void			handle_process_loop(t_command *cmd, char ***envp,
+					int *last_status);
+int				is_builtin(char *cmd);
+int				execute_builtin(t_command *cmd, char ***envp,
+					int *last_status);
+void			run_single_builtin(t_command *cmd, char ***envp,
+					int *last_status);
+int				handle_redirections(t_command *cmd, t_exec_params *p);
+void			wait_for_children(pid_t last_pid, int *last_status);
+void			execute_command_node(t_command *curr, char ***envp,
+					int *last_status);
+void			child_process(t_command *cmd, t_exec_params *params);
+int				handle_in(t_redir *redir);
+int				handle_out(t_redir *redir);
+int				handle_heredoc(t_redir *redir, t_exec_params *params);
+int				is_dir(char *path);
+void			exec_script(char *path, char **args, char **envp);
 
 /*
  * Builtins
  */
-int						ft_echo(char **args);
-int						ft_pwd(void);
-int						ft_cd(char **args, char ***envp);
-int						ft_env(char **envp);
-int						ft_export(char **args, char ***envp);
-int						ft_unset(char **args, char ***envp);
-int						ft_exit(char **args, int *last_status);
-int						is_all_digits(char *str);
-long long				ft_atoll(const char *str);
+int				ft_echo(char **args);
+int				ft_pwd(void);
+int				ft_cd(char **args, char ***envp);
+int				ft_env(char **envp);
+int				ft_export(char **args, char ***envp);
+int				ft_unset(char **args, char ***envp);
+int				ft_exit(char **args, int *last_status);
+int				is_all_digits(char *str);
+long long		ft_atoll(const char *str);
 
 /*
  * Cleanup
  */
-void					free_command_list(t_command *cmd_list);
-void					free_split(char **split);
-void					print_export(char **envp);
+void			free_command_list(t_command *cmd_list);
+void			free_split(char **split);
+void			print_export(char **envp);
 
 /*
  * Environment Utilities
  */
-char					**copy_env(char **envp);
-char					*get_env_value(char **envp, char *key);
-void					set_env(char ***envp, char *key, char *value);
-void					increment_shlvl(char ***envp);
-char					*get_var_value(char *name, char **envp,
-							int last_status);
-void					remove_var(char ***envp, int j);
+char			**copy_env(char **envp);
+char			*get_env_value(char **envp, char *key);
+void			set_env(char ***envp, char *key, char *value);
+void			increment_shlvl(char ***envp);
+char			*get_var_value(char *name, char **envp, int last_status);
+void			remove_var(char ***envp, int j);
 
 /*
  * Signals
  */
-void					setup_signals(void);
+void			setup_signals(void);
 
 #endif
